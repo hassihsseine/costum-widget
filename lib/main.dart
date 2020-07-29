d733e19f50d6454bc74a2b907e98ef12600a0e89
mohsen <houssamhassi13@gmail.com>
2020-07-29 16:21:40 +0100

    init costum text field, flat button

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/pages/login_page.dart';
+import 'package:instagram/pages/splash_page.dart';
+import 'package:instagram/pages/timeline_page.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram',
+      routes: <String, WidgetBuilder>{
+        "/": (context) => SplashPage(),
+        "/login": (context) => LoginPage(),
+        "/timeline": (context) => TimelinePage(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
